20b7742d81a9a40e8573752b1551a78d044934ab
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-06-21 16:54:58 +0300

    Добавляет основную разметку и стили

---
+++ src/js/slider.js
@@ -0,0 +1,21 @@
+import Splide from '@splidejs/splide';
+
+new Splide('#splide', {
+  // pagination: 'false',
+  // arrows: 'slider',
+}).mount();
+
+new Splide('#splide2', {
+  // pagination: 'false',
+  // arrows: 'slider',
+}).mount();
+
+new Splide('#splide3', {
+  // pagination: 'false',
+  // arrows: 'slider',
+}).mount();
+
+new Splide('#splide4', {
+  // pagination: 'false',
+  // arrows: 'slider',
+}).mount();
